3cb6836206ddbb5c9d4c26e1e4b13c1d95c83f38
AdityaPrakash1998 <abekh19@gmail.com>
2021-02-14 17:25:26 +0530

    Initial Commit. Almost Done

---
+++ README.md
@@ -0,0 +1,5 @@
+## React based Landing Page of Stripe
+
+## Check It Out
+
+[aditya-react-stripe-landing.netlify.app](aditya-react-stripe-landing.netlify.app)
---
+++ src/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import phoneImg from './images/phone.svg'
+import {useGlobalContext} from "./context"
+const Hero = () => {
+  const {closeSubmenu} = useGlobalContext();
+
+  return <section className="hero" onMouseOver={closeSubmenu}>
+    <div className="hero-center">
+      <article className="hero-info">
+        <h1>Payment Infrastructure for the internet</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores est vel dolorem beatae nulla quam pariatur, soluta totam minima ad.
+        </p>
+        <button className="btn">Start Now</button>
+      </article>
+      <article className="hero-images">
+        <img src={phoneImg} alt="Phone" className="phone-img"/>
+      </article>
+    </div>
+  </section>
+}
+
+export default Hero
